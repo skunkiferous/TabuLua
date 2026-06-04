28d02156a372c2e2a2ccce0b9e1e9fccfaa33a77
Sebastien Diot <monster@shadowdemon.com>
2026-06-04 10:06:49 +0200

    Version 0.23.0

--- importer.lua
+++ importer.lua
@@ -5,7 +5,7 @@ local NAME = "importer"
 local semver = require("semver")
 
 -- Module version
-local VERSION = semver(0, 22, 0)
+local VERSION = semver(0, 23, 0)
 
 -- Dependencies
 local read_only = require("read_only")
--- sandbox_env.lua
+++ sandbox_env.lua
@@ -5,7 +5,7 @@ local NAME = "sandbox_env"
 local semver = require("semver")
 
 -- Module version
-local VERSION = semver(0, 22, 0)
+local VERSION = semver(0, 23, 0)
 
 local read_only = require("read_only")
 local readOnly = read_only.readOnly
